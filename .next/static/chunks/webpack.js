13a8373cebe8aa95faa26196c0762f6c55aedecb
Svetlana Savchenko <ssavchenko8@gmail.com>
2026-02-18 15:32:12 +0000

    Refactor code structure for improved readability and maintainability

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5665af06ced375a2"; }
+/******/ 		__webpack_require__.h = function() { return "72025bc703f6deec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
